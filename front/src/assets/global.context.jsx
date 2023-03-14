e76c7f2e793d9232c0a18bf0dee54f0cdb5996a8
sebasRT <rtsebas11@gmail.com>
2023-03-14 18:36:21 -0500

    Build error fixed

--- front/src/assets/global.context.jsx
+++ front/src/assets/global.context.jsx
@@ -2,20 +2,21 @@ import axios from "axios";
 import { createContext, useEffect, useState } from "react"
 import { useMedia } from "../hooks/useMedia";
 
-const url = "http://3.128.29.96:8080/"
+const url = "http://localhost:8080/"
 
-  const requestProducts = await axios.get(`${url}producto`)
+  
+  const requestProducts =async()=> await axios.get(`${url}producto`)
   .then(result => result.data)
   .catch((err) => {
-    console.log(err);
-  });
+    console.log(err);})
 
-  const requestCategories = await axios.get(`${url}categoria`)
-  .then(result =>result.data)
-  .catch((err) => {
-    console.log(err);
-  });
-  
+
+  const requestCategories =async()=> await axios.get(`${url}categoria`)
+    .then(result =>result.data)
+    .catch((err) => {
+      console.log(err);})
+
+
   
 
 export const GlobalContext = createContext();
@@ -26,8 +27,20 @@ export const GlobalProvider = ({children}) => {
   const [categories, setcategories] = useState("")
 
   useEffect(() => {
-    setProducts(requestProducts);
-    setcategories(requestCategories)
+    async function getProducts (){
+      const response = await axios.get(`${url}producto`)
+      console.log(response)
+      setProducts(response.data)
+    }
+    getProducts()
+
+    async function getCategories (){
+      const response = await axios.get(`${url}categoria`)
+      console.log(response)
+      setcategories(response.data)
+    }
+    getCategories()
+    // async function 
   }, [])
   
   return (
