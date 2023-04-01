088c8a30ff50b4a99255d81b9b8befd9d7f497ad
fabriconiglio <fconiglio100@gmail.com>
2023-03-31 22:13:36 -0300

    feat: subo cors actualizado(2)

--- back/src/main/java/com/example/booking/segurity/WebSecurityConfig.java
+++ back/src/main/java/com/example/booking/segurity/WebSecurityConfig.java
@@ -12,9 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @Configuration
-@EnableWebMvc
 @AllArgsConstructor
 public class WebSecurityConfig {
 
@@ -62,4 +64,17 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    //Habilitar CORS para el front
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
