f1ce1ae9749ee07e6dd123516d865e1ff5a0d160
sebasRT <rtsebas11@gmail.com>
2023-04-05 17:30:10 -0500

    quito cambios de David

--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -4,13 +4,15 @@ import "../styles/Menu.css"
 import { useLogged } from '../hooks/useLogged';
 import { useMedia } from '../hooks/useMedia';
 
-export const Menu = () => {
+
+
+export const Menu = ({children}) => {
     const [open, setOpen] = useState(false)
-    const openHandle = () => setOpen(prev => !prev)
-    const { logged, name, admin } = useLogged();
+    const openHandle = ()=> setOpen(prev => !prev)
+    const {logged,name,admin} = useLogged();
     const isMobile = useMedia();
-    const [nombre, setnombre] = useState(name)
-    const closeSession = () => {
+
+    const closeSession = ()=>{
       localStorage.removeItem("name")
       localStorage.removeItem("email")
       localStorage.removeItem("password")
@@ -21,54 +23,33 @@ export const Menu = () => {
       const closeDropDown = e => {
         if (e.target.id != "menuButton") {
             setOpen(false);
-        }
-      }
+        }}
 
       document.body.addEventListener("click",closeDropDown);
       return () => {
         document.body.removeEventListener("click",closeDropDown)
       }
-    }, [])
+}, [])
     
-    return (
-      <div className='menu'>
-  {logged && (
-  <div className="user-info" style={{ alignItems: 'center' }}>
-    <div style={{ backgroundColor: '#263238', borderRadius: '50%', width: '50px', height: '50px', marginRight: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <span style={{ fontSize: '25px', fontWeight: 'bold', textTransform: 'uppercase', color: 'white' }}>
-        {nombre.substring(0, 1)}
-      </span>
-    </div>
-    <div className="user-name" style={{ fontSize: '18px' }}>
-      <p style={{ fontSize: '15px', color: 'rgba(0, 0, 0, 0.5)', fontWeight: 'bold'}}>Hola,</p>
-      {name} {localStorage.getItem('apellido') ? localStorage.getItem('apellido') : ''}
-    </div>
-  </div>
-)}
+  return (
+    <div className='menu'>
+      {children}
+      <button className='menuButton' onClick={openHandle} ><img src="./src/icons/menu.svg" id="menuButton"/></button>
+      <div className={`dropdown-menu ${open? "active": "unactive"}`} style={{width: `${isMobile?"80vw": "20vw"}`}} id="dm">
+        {
+          logged ? (<>
+          <Button1 text={name.toLocaleUpperCase()} ></Button1>
+          {admin?  <Button1 text="Edita Productos" link={`admin`}></Button1>: <Button1 text="Mis reservas" link={`bookings`}></Button1>}
+          <button onClick={closeSession}>cerrar sesión</button>
+          </>
+                      ):(<>
+          <Button1 link={'/login'} text={"login"}></Button1>
+          <Button1 link={'signUp'} text={'sign up'}></Button1>
+          </>)
 
-
-        <div className="menu-button-container">
-          <button className='menuButton' onClick={openHandle} >
-            <img src="./src/icons/menu.svg" id="menuButton"/>
-          </button>
-          <div className={`dropdown-menu ${open? "active": "unactive"}`} style={{width: `${isMobile?"80vw": "20vw"}`}} id="dm">
-            {
-              logged ? (
-                <>
-                  {admin?  <Button1 text="Edita Productos" link={`admin`}></Button1>: <Button1 text="Mis reservas" link={`bookings`}></Button1>}
-                  <button onClick={closeSession} type="button" className="botonCerrar">cerrar sesión</button>
-                </>
-              ) : (
-                <>
-                  <Button1 link={'/login'} text={"login"}></Button1>
-                  <Button1 link={'signUp'} text={'sign up'}></Button1>
-                </>
-              )
-            }
-          </div>
+        }
         </div>
-      </div>
-    )
-    
-    
+    </div>
+
+  )
 }
